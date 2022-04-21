c5ad52ccfb4442356f31edd7d88bd4d154963a73
BajcarM <102542768+BajcarM@users.noreply.github.com>
2022-04-21 13:10:00 +0200

    click handling, winner checking, console logs the winner

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
+let playerTurn = "cross";
+
+gameBoardCreate(10, 10);
+
+document.querySelectorAll(".tile").forEach(function (tileClicked) {
+  tileClicked.addEventListener("click", function (e) {
+    if (e.currentTarget.classList.contains("empty")) {
+      e.currentTarget.classList.add(playerTurn);
+      e.currentTarget.classList.remove("empty");
+      victoryChecker(e.currentTarget.id);
+      roundSwitch();
+    }
+    console.log(e.currentTarget.id);
+  });
+});
+
 function gameBoardCreate(rows, columns) {
   const gameBoardContainer = document.getElementById("game-board-container");
   gameBoardContainer.innerHTML = "";
   for (i = 0; i < rows; i++) {
     for (j = 0; j < columns; j++) {
       const tile = document.createElement("div");
-      tile.className = "tile";
+      tile.className = "tile empty";
       tile.id = "" + i + j;
       gameBoardContainer.appendChild(tile);
     }
@@ -15,32 +31,34 @@ function victoryChecker(targetTileId) {
   let row = Number(targetTileId[0]);
   let col = Number(targetTileId[1]);
   let tile;
-  //   Row check
+  let fiveInRow = [0, 0, 0, 0];
+
   for (i = 0; i < 9; i++) {
-    tile = document.getElementById("" + row + (col - 4 + i));
-    console.log(tile);
+    check(0, "" + row + (col - 4 + i));
+    check(1, "" + (row - 4 + i) + col);
+    check(2, "" + (row - 4 + i) + (col - 4 + i));
+    check(3, "" + (row - 4 + i) + (col + 4 - i));
   }
 
-  //   Column check
-  for (i = 0; i < 9; i++) {
-    tile = document.getElementById("" + (row - 4 + i) + col);
-    console.log(tile);
-  }
-
-  //   diagonal check
-
-  for (i = 0; i < 9; i++) {
-    tile = document.getElementById("" + (row - 4 + i) + (col - 4 + i));
-    console.log(tile);
-  }
-
-  //   other diagonal check
-  for (i = 0; i < 9; i++) {
-    tile = document.getElementById("" + (row - 4 + i) + (col + 4 - i));
-    console.log(tile);
+  function check(index, coords) {
+    tile = document.getElementById(coords);
+    if (tile) {
+      if (tile.classList.contains(playerTurn)) {
+        fiveInRow[index]++;
+      } else {
+        fiveInRow[index] = 0;
+      }
+      if (fiveInRow[index] === 5) {
+        console.log(playerTurn + " Wins!");
+      }
+    }
   }
 }
 
-gameBoardCreate(10, 10);
-
-victoryChecker("44");
+function roundSwitch() {
+  if (playerTurn === "cross") {
+    playerTurn = "circle";
+  } else {
+    playerTurn = "cross";
+  }
+}
